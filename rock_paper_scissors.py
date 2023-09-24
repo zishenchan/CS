9c56768079bfa7e00c16cf97ae5a10a845f9aeb9
Sam Chan <96612600+zishenchan@users.noreply.github.com>
2023-09-24 15:21:08 +1000

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,22 +1,29 @@
 # rock_paper_scissors, you play against with computer
 import random
 
-player_choice = str(input("r for Rock, p for Paper, s for scissors, now make your chocice \
-    (r/p/s): "))
-# computer choose a random choice
-opponent_choice = random.choice(["r","p","s"])
 # define a function for rules, the rule is 'r > s, s > p, p > r'
 def play(player,opponent):
-    if (player=="r" and opponent =="s") or (player=="s" and opponent=="p") or \
-        (player=="p" and opponent == "r"):
-        return True
-    return False
+if (player=="r" and opponent =="s") or (player=="s" and opponent=="p") or \
+    (player=="p" and opponent == "r"):
+    return True
+return False
 
-# situation for tie game
-if player_choice == opponent_choice:
-    print("tie game")
-else:
-    # call the rule function and output
-    if play(player_choice,opponent_choice)==True:
-        print(f"You win, computer \'choice is {opponent_choice}")
-    print(f"You lost, computer \'choice is {opponent_choice}")
+# make a for loop if tie game
+while True:
+
+    player_choice = str(input("r for Rock, p for Paper, s for scissors, now make your chocice \
+        (r/p/s): "))
+
+    # computer choose a random choice
+    opponent_choice = random.choice(["r","p","s"])
+
+    # situation for tie game
+    if player_choice == opponent_choice:
+        print("tie game")
+        continue
+    else:
+        # call the rule function and output
+        if play(player_choice,opponent_choice)==True:
+            print(f"You win, computer \'choice is {opponent_choice}")
+        print(f"You lost, computer \'choice is {opponent_choice}")
+        break
